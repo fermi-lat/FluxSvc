a6418381e1d2fd1e4eaba8c0f389b85b8525b60e
cohen <>
2002-02-14 16:00:06 +0000

    time is passed as argument to Spectrum::energySrc, so that randomly drawed kinetic energies of particles can be time dependant

--- FluxSvc/Spectrum.h
+++ FluxSvc/Spectrum.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/FluxSvc/Spectrum.h,v 1.5 2002/01/30 01:36:44 srobinsn Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/FluxSvc/Spectrum.h,v 1.6 2002/02/07 23:51:02 srobinsn Exp $
 //
 //
 // Spectrum: base class for energy spectrum objects
@@ -62,7 +62,7 @@ public:
     virtual std::pair<float,float> dir(float energy)const;
     
     /// new interface for Hirosima classes
-    virtual double energySrc(HepRandomEngine* engine);
+    virtual double energySrc(HepRandomEngine* engine, double time=0);
     virtual std::pair<double,double> dir(double energy, HepRandomEngine* engine);
     
     
