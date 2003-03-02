fde1690f64ce620764d8ec415f1e11f7b28e2192
cvs2git <>
2003-03-02 06:36:00 +0000

    This commit was manufactured by cvs2svn to create tag 'v6r3'.
    Sprout from master 2003-03-02 06:35:58 UTC burnett 'final fix for moving headers'
    Cherrypick from master 2003-02-22 14:38:04 UTC burnett '':
        FluxSvc/EventSource.h
        FluxSvc/FluxSource.h
    Cherrypick from GLAST 2003-03-02 06:35:59 UTC burnett 'AutoUpdate':
        ChangeLog
        cmt/Makefile
        doc/.cvsignore

---
+++ FluxSvc/EventSource.h
@@ -0,0 +1,116 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/FluxSvc/EventSource.h,v 1.14 2003/02/22 05:42:24 burnett Exp $
+#ifndef EventSource_h
+#define EventSource_h 1
+
+/** 
+* \class EventSource
+*
+* \brief  Base class for managing  sources.
+* 
+* $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/FluxSvc/EventSource.h,v 1.14 2003/02/22 05:42:24 burnett Exp $
+*/
+
+
+#include <string>
+
+class DOM_Element;
+class FluxSource;
+
+
+class EventSource
+{
+public:
+    /// ctor/dtor
+    EventSource (double aFlux = 1.0, unsigned acode = 0);
+    EventSource (const DOM_Element& xelem);
+    virtual ~EventSource();
+    
+    ///    a randomized interval to the next event - default is 1/rate()
+    virtual double interval (double) = 0;
+    
+    ///    calculate the rate for a given flux/solid angle integral (NOTE: integral of solid angle)
+    // virtual double  rate ( double solid_angle, double flux );	
+    virtual double  rate (double time)const;
+    virtual void    setRate ( double );
+    
+    ///    abstract method - create an event
+    virtual FluxSource* event (double) = 0;	  
+    
+    ///    UI titles - used for tuple header (verbose) or window title (display)
+    virtual std::string fullTitle () const;
+    virtual std::string displayTitle () const;
+    
+    ///    flux for this source in (p/(m^2*sr*sec))
+    virtual double	flux (double time) const;
+    virtual void      setFlux (double value);
+    
+    ///    disable/enable, test this particular source 
+    void      disable (){m_enabled=false;}
+    void      enable(){m_enabled=true;}
+    bool enabled()const{return m_enabled;}
+    
+    ///    integral of solid angle over which flux is incident
+    virtual double	solidAngle () const;
+    
+    ///	name of this flux source - for UI
+    const std::string& name () const;
+    void name (const std::string& value);
+    
+    ///    code - for monte-carlo study
+    unsigned  code () const;
+    virtual void  code ( unsigned );
+    
+    ///    area 
+    static double	totalArea ();
+    static void	totalArea ( double value );
+    
+    
+    /// virtual event number: should be filled in by subclass
+    virtual int eventNumber()const{return -1;} 
+    
+    ///  say which source created the current particle
+    virtual std::string findSource()const{return "";}
+    
+    
+    /// return a unique number correcponding to that spectrum
+    virtual int numSource()const{return -1;}
+    
+    virtual double time()const{return m_time;}
+    virtual void setTime(double time){m_time=time;}
+    
+    
+    //return how many sources are in the sourcelist (defaults to 1 if only a single FluxSource)
+    virtual int howManySources(){return 1;}
+
+    /// write the characteristics of the current source distribution to a stream
+    virtual void writeSourceCharacteristic(std::ostream& out){
+        out << fullTitle() << std::endl;
+        out<< "default message - no sources" << std::endl;
+    }
+    
+private:
+    double m_time;    // elapsed time, really only needed for EventSource
+    
+    bool m_enabled;           // toggle that it is enabled
+    double m_flux;		// representative flux for this event source...
+    std::string m_name;       // name of the event source (UI)
+    unsigned  m_code;         // code id for this event source - for identification in the tuple.
+    
+    static unsigned int  s_id;    // id for new EventSources...
+    static double s_total_area;   // total area for flux generation (in square meters)
+    double m_solid_angle;
+};
+
+// inline function declarations:
+
+
+inline const std::string& EventSource::name () const	{   return m_name;  }
+inline void EventSource::name (const std::string& value)    { m_name = value;   }
+
+inline double    EventSource::totalArea () { return s_total_area; }
+inline void    EventSource::totalArea (double value) { s_total_area = value; }
+
+inline unsigned EventSource::code () const { return m_code; }
+inline void EventSource::code ( unsigned c ) { m_code = c; }
+
+#endif
---
+++ FluxSvc/FluxSource.h
@@ -0,0 +1,114 @@
+/** @file FluxSource.h
+    @brief FluxSource declaration
+    */
+#ifndef FluxSource_h
+#define FluxSource_h 1
+
+#include "FluxSvc/EventSource.h"
+#include "CLHEP/Geometry/Point3D.h"
+#include "CLHEP/Geometry/Vector3D.h"
+
+// forward declarations
+class DOM_Element;
+class ISpectrum;
+
+// 
+/** @class FluxSource
+    @brief class which manages to compute flux from various particle source configurations
+    $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/FluxSvc/FluxSource.h,v 1.28 2003/02/22 05:42:24 burnett Exp $
+*/
+class FluxSource : public EventSource  
+{
+public:      
+    /**  constructor
+      @param xelem The xml description for this source
+      */
+    FluxSource ( const DOM_Element& xelem );
+#if 0 // are these needed anywhere?
+    FluxSource(double aFlux, ISpectrum* aSpec,  double l, double b);
+    FluxSource ( ISpectrum* aSpec = 0, double aFlux = 0 );
+#endif
+
+    ///    destructor
+    virtual ~FluxSource();
+
+    ///    generate an event 
+    virtual FluxSource* event(double time);
+
+    ///    full-length title description of this EventSource.
+    virtual std::string fullTitle () const;
+
+    ///    brief title description (for display) for this event source
+    virtual std::string displayTitle () const;
+
+    virtual double flux(double time)const; // calculate flux for attached spectrum
+
+    virtual double rate(double time)const; // calculate rate for attached spectrum
+
+    /// return a title describing the spectrum and angles
+    std::string title()const;
+
+    /// print facility
+    void  printOn ( std::ostream&  ) {}
+
+    /// set spectrum, with optional parameter to set the maximum energy?
+    void spectrum(ISpectrum* s, double emax=-1);
+
+    ISpectrum* spectrum() const{ return m_spectrum; }
+
+    double interval(double ){return m_interval;}
+
+    //! Denotes what Energy Units the energy
+    //! of incoming particles are in
+    enum EnergyScale { 
+        MeV,        //! MeV
+        GeV         //! GeV
+    } m_energyscale;
+
+    virtual int eventNumber()const;
+
+    double energy()const { return m_energy;}
+    const HepVector3D& launchDir()const {return m_correctedDir;}
+    const HepPoint3D&  launchPoint()const { return m_launchPoint;}
+
+private:
+
+    // forward declaration of classes that handle the lauch direction
+    class LaunchDirection;  // base class
+    class RandomDirection;  // choose randomly from range 
+    class SourceDirection;  // choose from an external source class
+
+    // forward declaration of classes that handle launch point
+    class LaunchPoint;  // base class
+    class RandomPoint; // random strategy
+    class FixedPoint;  // fixed, or pencil
+    class Patch;  // a box
+
+
+    LaunchPoint* m_launch_pt; // pointer to actual point stategy: must be set
+    LaunchDirection* m_launch_dir;
+
+    ISpectrum*         m_spectrum;	    // spectrum to generate
+
+    double m_energy;
+    double m_interval;
+    // associated with a specific launch
+
+    /// result of strategy
+    HepVector3D m_launchDir;
+
+    ///direction after being corrected for the "tilt" angles.
+    HepVector3D m_correctedDir;
+
+    HepPoint3D  m_launchPoint;
+
+    double calculateInterval (double time);
+
+    ///interval function to be used by non-spectrum sources
+    double explicitInterval (double time);
+    ///    getLaunch - compute launch point, direction, & energy
+    virtual void computeLaunch (double time=0);
+
+
+};
+#endif
