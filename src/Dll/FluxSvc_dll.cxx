6f8a66267a7039a8b2ef634f8210f6e3b8f11271
hansl <>
2004-07-22 01:02:37 +0000

    replace CalDigi by FluxSvc

--- src/Dll/FluxSvc_dll.cxx
+++ src/Dll/FluxSvc_dll.cxx
@@ -1,10 +1,10 @@
 /** @file FluxSvc_dll.cxx
     @brief declare DLL entry point
 
-    $Header$
+    $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_dll.cxx,v 1.4 2004/07/14 21:14:10 burnett Exp $
 
 */
 
 #include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(CalDigi)
+LOAD_FACTORY_ENTRIES( FluxSvc )
 
