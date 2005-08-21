975b8f75f4db490949beced7a141708f525eb1cf
burnett <>
2005-08-21 23:20:24 +0000

    tagging after bug fix

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/mainpage.h,v 1.16 2005/06/15 21:45:18 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/mainpage.h,v 1.17 2005/07/26 17:14:06 burnett Exp $
 // Mainpage for doxygen
 
 /*! \mainpage package FluxSvc
@@ -20,7 +20,8 @@ Usage is primarily via the FluxAlg algorithm, which access the service to genera
     @param FluxSvc.source_lib   [\$(FLUXSVCROOT)/xml/source_library.xml] list of file names containing source_lib elements
     @param FluxSvc.dtd_file     [\$(FLUXSVCROOT)/xml/source.dtd]  DTD file used to parse the XML files 
     @param FluxSvc.EvtMax       [0]  If non-zero, used as a maximum in the FluxSvc loop
-    @param FluxSvc.StartTimeEnvVar [""] If set (e.g., to "runName") use this as the source of the start time
+    @param FluxSvc.StartTimeEnvVar [""] If set (e.g., to "runName") use this as the source of the start time.
+    It has two forms: a number, or optionally a number for the start time offset, followed by a comma ant the delta time
     @param FluxSvc.StartTime    [0]  Mission elapsed time start
     @param FluxSvc.EndTime      [0]  Mission elapsed time end--if non-zero, will be the end.
     @param FluxSvc.DeltaTime    [0]  Maximum elapsed time.
