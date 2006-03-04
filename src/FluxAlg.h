601c0d36877e93aa37250701452d4317b512b979
cvs2git <>
2006-03-04 22:25:36 +0000

    This commit was manufactured by cvs2svn to create tag 'v0'.
    Sprout from master 2006-03-04 22:25:35 UTC burnett 'inital test package'
    Delete:
        .cvsignore
        ChangeLog
        FluxSvc/IFlux.h
        FluxSvc/IFluxSvc.h
        FluxSvc/IRegisterSource.h
        FluxSvc/ISpectrum.h
        FluxSvc/ISpectrumFactory.h
        FluxSvc/PointingInfo.h
        cdf/particle.cdf
        cmt/.cvsignore
        cmt/Makefile
        cmt/requirements
        doc/.cvsignore
        doc/Doxyfile
        doc/FluxSvcDoc2.htm
        doc/FluxSvcFig1.jpg
        doc/FluxSvcFig2.jpg
        doc/release.notes
        src/Dll/FluxSvc_dll.cxx
        src/Dll/FluxSvc_load.cxx
        src/ExposureAlg.cxx
        src/ExposureAlg.h
        src/FluxAlg.cxx
        src/FluxAlg.h
        src/FluxSvc.cxx
        src/FluxSvc.h
        src/FluxSvcRandom.cxx
        src/PointInfoAlg.cxx
        src/PointingInfo.cxx
        src/defaultOptions.txt
        src/mainpage.h
        src/test/FluxTestAlg.cxx
        src/test/flux/TimeDepSpectrum.cxx
        src/test/flux/TimeDepSpectrum.h
        src/test/flux/roottest.cxx
        src/test/flux/testMgr.cxx
        src/test/flux/test_library.xml
        src/test/jobOptions.txt
        src/test/testOptions.txt
        src/test/test_source.xml

--- src/FluxAlg.h
+++
@@ -1,6 +0,0 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/FluxAlg.h,v 1.7 2002/12/17 07:43:12 srobinsn Exp $
-#ifndef FluxAlg_h
-#define FluxAlg_h
-// see the source file FluxAlg.cxx for the class declaration
------------------------------------------------------------------
-#endif
