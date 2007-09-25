4f7f6448af58635dca023e24e311980010f99a92
burnett <>
2007-09-25 02:07:34 +0000

    Add new service to set orbit parameters; tag

--- src/Dll/FluxSvc_load.cxx
+++ src/Dll/FluxSvc_load.cxx
@@ -3,13 +3,14 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.13 2003/07/11 23:09:33 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.14 2005/09/20 15:05:19 burnett Exp $
 */
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 
 DECLARE_FACTORY_ENTRIES(FluxSvc) {
     DECLARE_SERVICE( FluxSvc );
+    DECLARE_SERVICE( OrbitSvc );
     DECLARE_ALGORITHM( FluxAlg );
     DECLARE_ALGORITHM( ExposureAlg );
     DECLARE_ALGORITHM( PointInfoAlg );
