44ce36773cb71bce8a8db925f1c97ccc8cc3b40d
ecephas <>
2008-10-31 15:58:41 +0000

    Added the following shared library:
    	env.Tool('celestialSourcesLib')

--- FluxSvcLib.py
+++ FluxSvcLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/FluxSvc/FluxSvcLib.py,v 1.1 2008/08/15 21:42:34 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['FluxSvc'])
-
+    env.Tool('celestialSourcesLib')
 def exists(env):
     return 1;
