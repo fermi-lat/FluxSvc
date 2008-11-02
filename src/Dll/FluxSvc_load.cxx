97d2a64c970cc5373f42b6bea256350c911c1993
burnett <>
2008-11-02 00:45:36 +0000

    removing TU changes

--- src/Dll/FluxSvc_load.cxx
+++ src/Dll/FluxSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.15 2007/09/25 02:07:34 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.16 2008/05/15 20:14:25 usher Exp $
 */
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
@@ -15,6 +15,5 @@ DECLARE_FACTORY_ENTRIES(FluxSvc) {
     DECLARE_ALGORITHM( ExposureAlg );
     DECLARE_ALGORITHM( PointInfoAlg );
     DECLARE_TOOL( FluxSvcRandom );
-    DECLARE_TOOL( FluxPointingInfoTool );
 }
 
