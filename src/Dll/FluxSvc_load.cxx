36fda40f7677647149d048b67377419fa461d231
lsrea <>
2009-03-19 04:38:38 +0000

    comment the DECLARE statement for PointInfoAlg, which no longer exists

--- src/Dll/FluxSvc_load.cxx
+++ src/Dll/FluxSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.16 2008/05/15 20:14:25 usher Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Dll/FluxSvc_load.cxx,v 1.17 2008/11/02 00:45:34 burnett Exp $
 */
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
@@ -13,7 +13,8 @@ DECLARE_FACTORY_ENTRIES(FluxSvc) {
     DECLARE_SERVICE( OrbitSvc );
     DECLARE_ALGORITHM( FluxAlg );
     DECLARE_ALGORITHM( ExposureAlg );
-    DECLARE_ALGORITHM( PointInfoAlg );
+    // removed 18-Mar-2009
+    // DECLARE_ALGORITHM( PointInfoAlg );
     DECLARE_TOOL( FluxSvcRandom );
 }
 
